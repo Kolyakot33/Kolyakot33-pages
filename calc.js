2ec5d3431cff6dcd480de149521534921a95c3f7
kolyakot33 <kolyakot33@localhost.localdomain>
2021-10-10 21:50:19 +0300

    FIX

---
+++ calc.js
@@ -0,0 +1,14 @@
+let playerCount
+let pluginCount
+setInterval(e => {
+    if (parseInt(players.value) == playerCount && parseInt(plugins.value) == pluginCount) {
+        return
+    }
+    pluginCount = parseInt(plugins.value) ?? 0;
+    playerCount = parseInt(players.value) ?? 0;
+    if (isNaN(pluginCount) || isNaN(playerCount)) {
+        return
+    }
+    memory.textContent = Math.ceil((playerCount + pluginCount / 2.2) / 6.875)
+    cpu.textContent = Math.ceil((playerCount + pluginCount / 2.2) / 13.75)
+}, 500)
--- calc.md
+++ calc.md
@@ -6,11 +6,4 @@
 <h2 id="memory"></h2>
 <h1>Ядра процессора:</h1>
 <h2 id="cpu"></h2>
-<script>
-    setInterval(e => {
-    let pluginCount = parseInt(plugins.value) ?? 0;
-    let playerCount = parseInt(players.value) ?? 0;
-    memory.textContent = Math.round((playerCount + pluginCount) / 8.75)
-    cpu.textContent = Math.round((playerCount + pluginCount) / 17.5)
-}, 500)
-</script>
+<script src="./calc.js"></script>
